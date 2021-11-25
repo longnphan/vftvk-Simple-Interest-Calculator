9f75cc19b8c79fb2885650643b3654ff84963838
Long Phan <longnphan@outlook.com>
2021-11-25 11:45:02 -0700

    complete html, css, js files

--- script.js
+++ script.js
@@ -1,6 +1,40 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function compute() {
+    // Variables
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = (principal * years * rate) / 100;
+    var year = new Date().getFullYear() + parseInt(years);
+
+    // Validates if 'Amount' input is a positive number
+    if (principal < 1) {
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+    }
+
+    document.getElementById("result").innerHTML =
+        "If you deposit " +
+        "<mark>" +
+        principal +
+        "</mark>" +
+        ",<br>at an interest rate of " +
+        "<mark>" +
+        rate +
+        "</mark>" +
+        "%<br>You will receive an amount of " +
+        "<mark>" +
+        interest +
+        "</mark>" +
+        ",<br>in the year " +
+        "<mark>" +
+        year +
+        "</mark>" +
+        "<br>";
+    console.log(`this is the principal: ${principal}`);
 }
-        
+
+// Updates the interest rate when the value from the range slider input changes
+function updateRate() {
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText = rateval + "%";
+}
